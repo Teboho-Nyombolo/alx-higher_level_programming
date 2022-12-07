ec9129ad676cb87852055300496245e06b02a62f
Teboho Thulani Nyombolo <109901691+TatakaSibu@users.noreply.github.com>
2022-12-07 20:33:38 +0200

    All Tasks

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[])
-new_matrix = matrix.copy()
-for i in range(len(matrix)):
-new_matrix[i] = list(map(lambda x: x**2, matrix[i]))
-return (new_matrix)
+
+def square_matrix_simple(matrix=[]):
+    return [list(map(lambda x: x ** 2, row)) for row in matrix]
